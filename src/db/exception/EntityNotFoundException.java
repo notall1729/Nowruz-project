6f2b26087e9e20c7d77bc32222f627e06ba7f4cc
goli <shirafkanzeynab63@gmail.com>
2025-03-23 16:38:31 +0330

    add EntityNotFoundException constructors

--- src/db/exception/EntityNotFoundException.java
+++ src/db/exception/EntityNotFoundException.java
@@ -1,4 +1,16 @@
 package db.exception;
 
-public class EntityNotFoundException {
+public class EntityNotFoundException extends Exception{
+
+    EntityNotFoundException(){
+      super("Cannot find entity.");
+    }
+
+    EntityNotFoundException(String massage){
+        super(massage);
+    }
+
+    EntityNotFoundException(int id){
+        super("Cannot find entity with id = {" + id + "}");
+    }
 }
