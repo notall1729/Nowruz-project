e360a3ac68dd79b95921d8451b3fc4d999802519
goli <shirafkanzeynab63@gmail.com>
2025-03-27 03:25:07 +0330

    add method add to the class Database

--- src/db/Database.java
+++ src/db/Database.java
@@ -3,7 +3,10 @@ package db;
 import java.util.ArrayList;
 
 public class Database {
-    private ArrayList<Entity> entities = new ArrayList<>();
+    private static ArrayList<Entity> entities = new ArrayList<>();
 
-    
+    public static void add(Entity e){
+        e.id = entities.size() + 1;
+        entities.add(e);
+    }
 }
