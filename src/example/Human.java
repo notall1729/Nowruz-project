a8cc79a1e0fbf6d7f8290cf005ea58fe1a23107d
goli <shirafkanzeynab63@gmail.com>
2025-03-27 03:55:32 +0330

    add an abstract method copy to the Entity class

--- src/example/Human.java
+++ src/example/Human.java
@@ -6,4 +6,12 @@ public class Human extends db.Entity {
     public Human(String name) {
         this.name = name;
     }
+
+    @Override
+    public Human copy() {
+        Human copyHuman = new Human(name);
+        copyHuman.id = id;
+
+        return copyHuman;
+    }
 }
