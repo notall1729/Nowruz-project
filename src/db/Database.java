57a24932403a4ce7c600ade7ee6acce1e29e396c
goli <shirafkanzeynab63@gmail.com>
2025-04-05 20:42:51 +0330

    update Database

--- src/db/Database.java
+++ src/db/Database.java
@@ -1,14 +1,21 @@
 package db;
 
 import db.exception.EntityNotFoundException;
+import db.exception.InvalidEntityException;
+
+import javax.crypto.spec.IvParameterSpec;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Database {
     private static ArrayList<Entity> entities = new ArrayList<>();
+    private static HashMap<Integer, Validator> validators = new HashMap<>();
 
-    public static void add(Entity e){
+    public static void add(Entity e) throws InvalidEntityException {
+       validateEntity(e);
+
         e.copy().id = entities.size() + 1;
-        entities.add(e);
+        entities.add(e.copy());
     }
 
     public static Entity get(int id) throws EntityNotFoundException{
@@ -30,7 +37,8 @@ public class Database {
         throw new EntityNotFoundException(id);
     }
 
-    public static void update(Entity e) throws EntityNotFoundException{
+    public static void update(Entity e) throws EntityNotFoundException, InvalidEntityException {
+        validateEntity(e);
         for (int i = 0; i < entities.size(); i++){
             if(entities.get(i).id == e.id){
                 entities.set(i, e.copy());
@@ -38,5 +46,19 @@ public class Database {
             }
         }
         throw new EntityNotFoundException();
+    }
+
+    public static void registerValidator(int entityCode, Validator validator){
+        if(validators.containsKey(entityCode)){
+            throw new IllegalArgumentException("Validator for entity code " + entityCode + "already exists.");
+        }
+        validators.put(entityCode, validator);
+    }
+
+    private static void validateEntity(Entity entity)throws InvalidEntityException{
+        Validator validator = validators.get(entity.getEntityCode());
+        if(validator != null){
+            validator.validate(entity);
+        }
     }
 }
