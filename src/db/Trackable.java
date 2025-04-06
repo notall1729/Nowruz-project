8ddb114f5155168b8097deac312ea9dc3742bb1b
goli <shirafkanzeynab63@gmail.com>
2025-04-06 09:24:59 +0330

    add Trackable commands

--- src/db/Trackable.java
+++ src/db/Trackable.java
@@ -1,4 +1,10 @@
 package db;
+import java.sql.Date;
 
 public interface Trackable {
+    void setCreationDate(Date date);
+    Date getCreationDate();
+
+    void setLastModificationDate(Date date);
+    Date getLastModificationDate();
 }
