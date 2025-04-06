e96fee8082a3ad8b4893bbf6398276541fda6675
goli <shirafkanzeynab63@gmail.com>
2025-04-06 09:40:16 +0330

    add Document commands

--- src/example/Document.java
+++ src/example/Document.java
@@ -1,4 +1,34 @@
 package example;
 
-public class Document {
+import db.Trackable;
+import java.sql.Date;
+
+public class Document implements Trackable, extends Entity{
+    private String content;
+    private Date creationDate;
+    private Date lastModificationDate;
+
+    public Document(String content){
+        this.content = content;
+    }
+
+    @Override
+    public void setCreationDate(Date date){
+        this.creationDate = date;
+    }
+
+    @Override
+    public Date getCreationDate(){
+        return;creationDate;
+    }
+
+    @Override
+    public void setLastModificationDate(Date date){
+        this.lastModificationDate = date;
+    }
+
+    @Override
+    public Date getLastModificationDate() {
+        return lastModificationDate;
+    }
 }
