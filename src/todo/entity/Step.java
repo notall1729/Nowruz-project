11c81d056e41c6301233abe091436e00cd3e9ccc
goli <shirafkanzeynab63@gmail.com>
2025-04-10 18:04:09 +0330

    add Step commands

--- src/todo/entity/Step.java
+++ src/todo/entity/Step.java
@@ -1,4 +1,30 @@
 package todo.entity;
+import db.Entity;
+public class Step extends Entity{
+    private String title;
+    public status status;
+    private int taskRef;
+    public static final int STEP_ENTITY_CODE = 29;
 
-public class Step {
+    public enum status{
+        NotStarted, Completed
+    }
+
+    public Step (String title, int taskRef){
+        this.title = title;
+        this.taskRef = taskRef;
+        this.status = status.NotStarted;
+    }
+
+    public Step copy(){
+        Step copyStep = new Step(title, taskRef);
+        copyStep.id = id;
+
+        return copyStep;
+    }
+
+    @Override
+    public int getEntityCode() {
+        return STEP_ENTITY_CODE;
+    }
 }
